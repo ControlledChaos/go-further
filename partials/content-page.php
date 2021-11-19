4d4e1ee45a6e0f28713791c150c1e7773c3950ca
ControlledChaos <greg@ccdzine.com>
2021-11-18 22:54:33 -0800

    Allow page title on front page.

---
+++ partials/content-page.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Get content template for post type: page
+ *
+ * Parent theme looks for this file.
+ *
+ * @package    Go_Further
+ * @subpackage Templates
+ * @category   Content
+ * @since      1.0.0
+ */
+
+get_template_part( 'templates/template-parts/content/content', 'page' );
---
+++ templates/template-parts/content/content-page.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Content template for post type: page
+ *
+ * @package    Go_Further
+ * @subpackage Templates
+ * @category   Content
+ * @since      1.0.0
+ */
+
+namespace GoFurther;
+
+// Alias namespaces.
+use GoFurther\Classes\Front as Front;
+
+?>
+
+<?php if ( has_post_thumbnail() ) : ?>
+	<figure class="post__thumbnail">
+		<?php the_post_thumbnail(); ?>
+	</figure>
+<?php endif; ?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<?php Front\tags()->page_title(); ?>
+
+	<div class="<?php \Go\content_wrapper_class( 'content-area__wrapper' ); ?>">
+		<div class="content-area entry-content">
+			<?php the_content(); ?>
+			<?php wp_link_pages(); ?>
+		</div>
+	</div>
+
+</article>
