61329e1dbc286b871da774ed2fcf46813f9c70ad
ControlledChaos <greg@ccdzine.com>
2021-11-19 15:33:37 -0800

    Add cover image template.
    Thus far only a duplicate of the parent's page.php template.

---
+++ templates/cover-image.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Cover image template
+ *
+ * The template for displaying full intro cover images
+ * on supported post types' singular pages.
+ *
+ * Template Name: Cover Image
+ * Template Post Type: page, post, event, product
+ *
+ * @package    Go_Further
+ * @subpackage Templates
+ * @category   Page Templates
+ * @since      1.0.0
+ */
+
+get_header();
+
+// Start the Loop.
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'partials/content', 'page' );
+
+	// If comments are open or we have at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+
+endwhile;
+
+get_footer();
