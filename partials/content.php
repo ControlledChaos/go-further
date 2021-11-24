1309f2621d85d36e88a9d8ffd1ad23d1e27fdff1
ControlledChaos <greg@ccdzine.com>
2021-11-23 16:27:58 -0800

    Post content templates.

---
+++ partials/content.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * Parent theme looks for this file.
+ *
+ * @package    Go_Further
+ * @subpackage Templates
+ * @category   Content
+ * @since      1.0.0
+ */
+
+get_template_part( 'templates/template-parts/content/content' );
