3e24e1363a841ee5ae7ca9216d49aadd0231071d
ControlledChaos <greg@ccdzine.com>
2021-11-24 09:47:27 -0800

    Added social link(s) to customizer section.

--- includes/classes/core/class-setup.php
+++ includes/classes/core/class-setup.php
@@ -35,6 +35,9 @@ class Setup {
 		// Add excerpts to pages for use in meta data.
 		add_action( 'init', [ $this, 'add_page_excerpts' ] );
 
+		// Add social links to the parent array.
+		add_filter( 'go_avaliable_social_icons', [ $this, 'get_available_social_icons' ] );
+
 		// Login title.
 		add_filter( 'login_headertext', [ $this, 'login_title' ] );
 
@@ -68,6 +71,26 @@ class Setup {
 	 */
 	public function add_page_excerpts() {
 		add_post_type_support( 'page', 'excerpt' );
+	}
+
+	/**
+	 * Supported social icons
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return array Returns social links from parent and child themes.
+	 */
+	public function get_available_social_icons( $social_icons ) {
+
+		$added_icons = [
+			'codepen' => [
+				'label'       => esc_html__( 'CodePen', 'go-further' ),
+				'icon'        => get_theme_file_path( 'assets/images/social/codepen.svg' ),
+				'placeholder' => 'https://codepen.com/user',
+			],
+		];
+
+		return array_merge( $social_icons, $added_icons );
 	}
 
 	/**
