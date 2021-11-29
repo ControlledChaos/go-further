98addb6a4faaa9cd44c6f93442b9f68ebd030700
ControlledChaos <greg@ccdzine.com>
2021-11-29 13:31:17 -0800

    Docblock edit.

--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 /**
  * The main template file
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Go
+ * @package    Go_Further
+ * @subpackage Templates
+ * @category   Content
+ * @since      1.0.0
  */
 
 // Get blog data.
@@ -39,7 +35,7 @@ if ( have_posts() ) {
 	// Start the Loop.
 	while ( have_posts() ) :
 		the_post();
-		get_template_part( 'partials/content' );
+		get_template_part( 'partials/content', get_post_type() );
 	endwhile;
 
 	// Previous/next page navigation.
