a7b0a0c5ce00d2659250b5bc1a06b9d0fdc95979
ControlledChaos <greg@ccdzine.com>
2021-12-08 22:11:28 -0800

    Remove dev hooks.

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -15,20 +15,12 @@ use GoFurther\Front as Front;
 
 if ( Front\has_active_footer_sidebars() ) :
 
-do_action( 'GoFurther\before_footer_sidebar' );
-
 ?>
 <aside id="footer-widgets" class="<?php echo Front\footer_widgets_class(); ?>" role="complementary">
 	<div class="footer-widgets-wrapper">
-		<?php
-		do_action( 'GoFurther\before_footer_widgets' );
-		Front\footer_widgets();
-		do_action( 'GoFurther\after_footer_widgets' );
-		?>
+		<?php Front\footer_widgets(); ?>
 	</div>
 </aside>
 <?php
-
-do_action( 'GoFurther\after_footer_sidebar' );
 
 endif;
