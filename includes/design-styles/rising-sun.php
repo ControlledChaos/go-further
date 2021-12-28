e476c81c011bc063b99dca7ea43e4b17aa2c8de0
ControlledChaos <greg@ccdzine.com>
2021-12-28 10:13:29 -0800

    Color scheme hex colors.

--- includes/design-styles/rising-sun.php
+++ includes/design-styles/rising-sun.php
@@ -46,8 +46,8 @@ function rising_sun() {
 				],
 				'two' => [
 					'label'      => _x( 'Cherry Blossom', 'color palette name', 'go-further' ),
-					'primary'    => '#c43b71',
-					'secondary'  => '#4c454e',
+					'primary'    => '#cd6691',
+					'secondary'  => '#65334b',
 					'tertiary'   => '#f9edf1',
 					'background' => '#ffffff',
 					'text'       => '#333333',
@@ -62,7 +62,7 @@ function rising_sun() {
 				'three' => [
 					'label'      => _x( 'Chrysanthemum', 'color palette name', 'go-further' ),
 					'primary'    => '#efa700',
-					'secondary'  => '#4c454e',
+					'secondary'  => '#d27112',
 					'tertiary'   => '#fffae0',
 					'background' => '#ffffff',
 					'text'       => '#333333',
@@ -77,7 +77,7 @@ function rising_sun() {
 				'four' => [
 					'label'      => _x( 'Koi Pond', 'color palette name', 'go-further' ),
 					'primary'    => '#d46408',
-					'secondary'  => '#4c454e',
+					'secondary'  => '#364c78',
 					'tertiary'   => '#edf2fb',
 					'background' => '#ffffff',
 					'text'       => '#333333',
@@ -91,8 +91,8 @@ function rising_sun() {
 				],
 				'five' => [
 					'label'      => _x( 'Wisteria', 'color palette name', 'go-further' ),
-					'primary'    => '#6e4aa7',
-					'secondary'  => '#4c454e',
+					'primary'    => '#7f67a4',
+					'secondary'  => '#44355c',
 					'tertiary'   => '#eee9e2',
 					'background' => '#ffffff',
 					'text'       => '#333333',
@@ -102,12 +102,12 @@ function rising_sun() {
 					'footer_background'    => '',
 					'footer_heading_color' => '',
 					'footer_text_color'    => '',
-					'social_icon_color'    => ''
+					'social_icon_color'    => '#7f67a4'
 				],
 				'six' => [
 					'label'      => _x( 'Maple', 'color palette name', 'go-further' ),
-					'primary'    => '#9c2743',
-					'secondary'  => '#4c454e',
+					'primary'    => '#9b2928',
+					'secondary'  => '#5b202e',
 					'tertiary'   => '#f6f2df',
 					'background' => '#ffffff',
 					'text'       => '#333333',
@@ -117,12 +117,12 @@ function rising_sun() {
 					'footer_background'    => '',
 					'footer_heading_color' => '',
 					'footer_text_color'    => '',
-					'social_icon_color'    => ''
+					'social_icon_color'    => '#9b2928'
 				],
 				'seven' => [
 					'label'      => _x( 'Tea House', 'color palette name', 'go-further' ),
-					'primary'    => '#776355',
-					'secondary'  => '#4c454e',
+					'primary'    => '#734c33',
+					'secondary'  => '#493628',
 					'tertiary'   => '#f8f3dd',
 					'background' => '#ffffff',
 					'text'       => '#333333',
@@ -132,12 +132,12 @@ function rising_sun() {
 					'footer_background'    => '',
 					'footer_heading_color' => '',
 					'footer_text_color'    => '',
-					'social_icon_color'    => ''
+					'social_icon_color'    => '#734c33'
 				],
 				'eight' => [
 					'label'      => _x( 'Volcano', 'color palette name', 'go-further' ),
-					'primary'    => '#63738d',
-					'secondary'  => '#4c454e',
+					'primary'    => '#6c778b',
+					'secondary'  => '#4d535f',
 					'tertiary'   => '#eceef1',
 					'background' => '#ffffff',
 					'text'       => '#333333',
@@ -147,12 +147,12 @@ function rising_sun() {
 					'footer_background'    => '',
 					'footer_heading_color' => '',
 					'footer_text_color'    => '',
-					'social_icon_color'    => '#393e51'
+					'social_icon_color'    => '#6c778b'
 				],
 				'nine' => [
 					'label'      => _x( 'Sashimi', 'color palette name', 'go-further' ),
 					'primary'    => '#e36116',
-					'secondary'  => '#4c454e',
+					'secondary'  => '#7b262a',
 					'tertiary'   => '#eceef1',
 					'background' => '#ffffff',
 					'text'       => '#333333',
@@ -162,7 +162,7 @@ function rising_sun() {
 					'footer_background'    => '',
 					'footer_heading_color' => '',
 					'footer_text_color'    => '',
-					'social_icon_color'    => '#393e51'
+					'social_icon_color'    => '#e36116'
 				],
 			],
 			'fonts'=> [
